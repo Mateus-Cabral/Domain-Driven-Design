0dffbb4bb27a3228248f8da2d191bc89e5f15d02
Logon Aluno <logonrmlocal@fiap.com.br>
2022-03-21 11:51:18 -0300

    Aula01
    Váriáveis, palavras reservadas e concatenação

--- src/aula01/HelloWorld.java
+++ src/aula01/HelloWorld.java
@@ -1,9 +1,46 @@
 package aula01;
 
+
+// Exemplo de comentário em uma linha
+
+/* Exemplo
+ * de
+ * comentário
+ * de várias
+ * linhas
+ */
+
+
 public class HelloWorld {
 	public static void main(String[] args) {
-	System.out.println("a");	
+		byte idade = 19;
+		double peso = 65;
+		String nome = "Hairo Newdawn";
+		/* Declaração de var - <tipo> <nome_var> = <valor_var>
+		*  Byte   - Int de 8  Bits
+		*  Shot   - Int de 16 Bits
+		*  Int    - Int de 32 Bits
+		*  Long   - Int de 64 Bits
+		* 
+		*  Float  -  . flutuante de 32 Bits
+		*  Double - . flutuante de 64 Bits
+		*  
+		*  Char - Caractere unicode de 16 Bits - Tem que ser 'a'
+		*  String - Cadeia de char - Se usa "ab"
+		*  
+		*  Boolean - True or False
+		*/
 		
+		System.out.println("Hello World");
+		/* System - Domínio do Comando
+		*  Out - Saída
+		*  print - Comando de Print
+		*  println - Print com linha após
+		*/
+		
+		System.out.println("Idade: " + idade);
+		System.out.println("Nome: " + nome);
+		System.out.println("Peso: " + peso);
+		// Concatenação em Java se usa o + e não o ,
 	}
-
 }
